0dad0408dc230581a99b524ac4e6924b9436cbb6
ascheben <scheben@cshl.edu>
2023-02-08 14:06:46 -0500

    add simulated test data

---
+++ data/simulated_data/generate_data.sh
@@ -0,0 +1,4 @@
+# commands used to generate simulated data
+# note that file suffix "fq" must be renamed to "fastq" for EvoTraceR
+./sim_wrapper.sh --out simsmall --mutrate1 0.1 --mutrate2 0.01 --max-indel-size 3 --samples 20
+./sim_wrapper.sh --out simmid --mutrate1 0.1 --mutrate2 0.05 --max-indel-size 5 --samples 50
