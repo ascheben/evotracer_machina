ff707c9ce712062f3ca344ae0fa5cf219321f1c5
Staklinski <staklins@evolgen.cshl.edu>
2023-04-21 10:19:54 -0400

    annotate input and verify function of parallel sim script

--- parallel_sim.sh
+++ parallel_sim.sh
@@ -8,15 +8,46 @@ equal_mm="data/equal_migration_prob_matrix.csv"
 true_mm="data/true_migration_prob_matrix.csv"
 
 # Set the mutation matrix filepaths
-#mm_array=(${rare_mm} ${equal_mm} ${true_mm})
-mm_array=(${equal_mm} ${true_mm})
+mm_array=(${equal_mm} ${true_mm} ${rare_mm})
 
 # Set the mutation rates to explore
 mr_array=(0.01 0.025 0.05 0.1 0.2)
 
+# Set the max indel sizes to explore
+mi_array=(5 10 15)
+
 # Set the sample sizes to explore
-#ss_array=(100 1000 10000 50000 100000)
 ss_array=(100 200)
+
+# Setup headers for recording the input parameters in a csv
+mm_header="mutation_matrix"
+mr_header="mutation_rate"
+mi_header="max_indel_size"
+ss_header="sample_size"
+
+# Determine the maximum length of the arrays for input parameter csv writing
+max_len=${#mm_array[@]}
+if [ ${#mr_array[@]} -gt $max_len ]; then
+  max_len=${#mr_array[@]}
+fi
+if [ ${#mi_array[@]} -gt $max_len ]; then
+  max_len=${#mi_array[@]}
+fi
+if [ ${#ss_array[@]} -gt $max_len ]; then
+  max_len=${#ss_array[@]}
+fi
+
+# Write the array values to a CSV file
+echo "${mr_header},${mi_header},${ss_header},${mm_header}" > input_ranges_${parallel_sim_name}.csv
+
+for ((i=0; i<$max_len; i++)); 
+  do
+    mr=${mr_array[$i]:-}
+    mi=${mi_array[$i]:-}
+    ss=${ss_array[$i]:-}
+    mm=${mm_array[$i]:-}
+    echo "${mr},${mi},${ss},${mm}" >> input_ranges_${parallel_sim_name}.csv
+  done
 
 # Create an array to store all the commands
 commands=()
@@ -27,31 +58,36 @@ for mm in ${mm_array[@]}
 do
   for mr in ${mr_array[@]}
   do
-    for ss in ${ss_array[@]}
+    for mi in ${mi_array[@]}
     do
-      # Construct the command to submit with ParaFly
-      mm_type=$(echo $mm | awk -F/ '{print $2}' | awk -F_ '{print $1}')
-      mr_value=$(echo $mr | awk -F. '{print $2}')
-      simmid="sim_${mr_value}_${ss}_${mm_type}"
-      cmd="./sim_full_pipeline.sh --out ${simmid} --mutrate ${mr} --max-indel-size 5 --samples ${ss} --migration ${mm}"
-
-      # Add the command to the commands array
-      commands+=("$cmd")
+      for ss in ${ss_array[@]}
+      do
+        # Construct the command to submit with ParaFly
+        mm_type=$(echo $mm | awk -F/ '{print $2}' | awk -F_ '{print $1}')
+        mr_value=$(echo $mr | awk -F. '{print $2}')
+        simmid="sim_${mr_value}_${mi}_${ss}_${mm_type}"
+        cmd="./sim_full_pipeline.sh --out ${simmid} --mutrate ${mr} --max-indel-size ${mi} --samples ${ss} --migration ${mm}"
+        # Add the command to the commands array
+        commands+=("$cmd")
+      done
     done
   done
 done
 echo "There are ${#commands[@]} commands to be submitted."
 
+# Create the main output file with the header
+echo "name,mutrate,max_indel_size,num_samples,migration_matrix,true_migrations,inferred_migrations,proportion" > output_all_${parallel_sim_name}.csv
+
 # Submit the commands in batches of 20 using ParaFly
-batch_size=1
-num_batches=$((${#commands[@]} / batch_size))
+batch_size=20
+batches=$((${#commands[@]} / batch_size))
+num_batches=$(echo "scale=0; ($batches+0.5)/1" | bc)
 
 echo "Beginning submission of all combinations in parallel batches of ${batch_size}..."
 conda activate machina
-#for ((i=0; i<$num_batches; i++))
-for ((i=0; i<2; i++))
+for ((i=0; i<$num_batches; i++))
 do
-  echo "Submitting batch ${i} of ${num_batches}:"
+  echo "Submitting batch $((i+1))/$((num_batches+1)):"
   # Calculate the range of commands to submit in this batch
   start_index=$((i * batch_size))
 
@@ -66,20 +102,17 @@ do
   ParaFly -CPU ${batch_size} -c ${i}.cmd
   rm ${i}.cmd
   rm ${i}.cmd.completed
+  # Append the second line of each csv file to the main file
+  for f in sim_results*/comparison*; do tail -n +2 $f >> output_all_${parallel_sim_name}.csv; done
+  # Remove all the individual csv files
+  rm -r sim_results*
 done
 
-echo "Commands are done, now appending results to one file..."
-
-# Create the main output file with the header
-echo "name,mutrate,max_indel_size,num_samples,migration_matrix,true_migrations,inferred_migrations,proportion" > ${parallel_sim_name}_all_output.csv
-
-# Append the second line of each csv file to the main file
-for f in sim_results*/comparison*; do tail -n +2 $f >> ${parallel_sim_name}_all_output.csv; done
-
-# Remove all the individual csv files
-rm -r sim_results*
-
-echo "Done."
+echo "All batches are done."
 
 conda deactivate
+
+par_results="${parallel_sim_name}_parallel_sim_results"
+mkdir ${par_results}
+mv *${parallel_sim_name}.csv ${par_results}/
 
