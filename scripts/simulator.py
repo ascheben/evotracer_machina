82894a523406383632c0861adf8aa28361e7e29b
Staklinski <staklins@evolgen.cshl.edu>
2023-04-26 08:52:38 -0400

    make new dropout mutation changes to simulator

--- scripts/simulator.py
+++ scripts/simulator.py
@@ -1,23 +1,103 @@
 #!/usr/bin/env python3
 import sys
-import cProfile
-from collections import defaultdict
-import copy
 import numpy as np
 import pandas as pd
-import time
+import math
 import random
-import cassiopeia as cas
 from ete3 import Tree
+import cassiopeia as cas
+from cassiopeia.data import CassiopeiaTree
 
-#import pdb
-#import networkx as nx
-#from tqdm.auto import tqdm
-#import matplotlib.pyplot as plt
-#from cassiopeia.solver import missing_data_methods
-#import dendropy
-#from dendropy.calculate import treecompare
-#from dendropy.simulate import treesim
+# monkey patch Cassiopeia to output mutation age
+def overlay_data_drop(self, tree: CassiopeiaTree):
+    """Overlays Cas9-based lineage tracing data onto the CassiopeiaTree.
+    Args:
+        tree: Input CassiopeiaTree
+    """
+    if self.random_seed is not None:
+        np.random.seed(self.random_seed)
+
+    # create state priors if they don't exist.
+    # This will set the instance's variable for mutation priors and will
+    # use this for all future simulations.
+    if self.mutation_priors is None:
+        self.mutation_priors = {}
+        probabilites = [
+            self.state_generating_distribution()
+            for _ in range(self.number_of_states)
+        ]
+        Z = np.sum(probabilites)
+        for i in range(self.number_of_states):
+            self.mutation_priors[i + 1] = probabilites[i] / Z
+
+    number_of_characters = self.number_of_cassettes * self.size_of_cassette
+
+    # initialize character states
+    character_matrix = {}
+    for node in tree.nodes:
+        character_matrix[node] = [-1] * number_of_characters
+
+    for node in tree.depth_first_traverse_nodes(tree.root, postorder=False):
+
+        if tree.is_root(node):
+            character_matrix[node] = [0] * number_of_characters
+            continue
+
+        parent = tree.parent(node)
+        life_time = tree.get_time(node) - tree.get_time(parent)
+
+        character_array = character_matrix[parent]
+        open_sites = [
+            c
+            for c in range(len(character_array))
+            if character_array[c] == 0
+        ]
+
+        new_cuts = []
+        for site in open_sites:
+            mutation_rate = self.mutation_rate_per_character[site]
+            mutation_probability = 1 - (np.exp(-life_time * mutation_rate))
+
+            if np.random.uniform() < mutation_probability:
+                new_cuts.append(site)
+
+        # collapse cuts that are on the same cassette
+        cuts_remaining = new_cuts
+        if self.collapse_sites_on_cassette and self.size_of_cassette > 1:
+            character_array, cuts_remaining = self.collapse_sites(
+                character_array, new_cuts
+            )
+        # introduce new states at cut sites
+
+        character_array = self.introduce_states(
+            character_array, cuts_remaining
+        )
+        if len(cuts_remaining) >0:
+            timestamp = round(tree.get_time(node),6)
+            for i in cuts_remaining:
+                character_array[i] = str(character_array[i]) + "_" + str(timestamp)
+            #print(node,tree.get_time(node),character_array, cuts_remaining)
+        # silence cassettes
+        silencing_probability = 1 - (
+            np.exp(-life_time * self.heritable_silencing_rate)
+        )
+        character_array = self.silence_cassettes(
+            character_array,
+            silencing_probability,
+            self.heritable_missing_data_state,
+        )
+
+        character_matrix[node] = character_array
+
+    # apply stochastic silencing
+    for leaf in tree.leaves:
+        character_matrix[leaf] = self.silence_cassettes(
+            character_matrix[leaf],
+            self.stochastic_silencing_rate,
+            self.stochastic_missing_data_state,
+        )
+
+    tree.set_all_character_states(character_matrix)
 
 def mutate_seq(seq,pos,mut_bases):
     '''
@@ -33,18 +113,20 @@ def mutate_seq(seq,pos,mut_bases):
         #mut_seq = seq[:pos-1] + mut_bases + seq[pos-1+mut_size:]
     return mut_seq
 
-def generate_indel(max_size):
+def generate_indel():
     '''
     Take sequence and position and randomly introduce an
     indel at the given position. Returns the mutated sequence.
     '''
-    del_prob = 0.6
-    ins_prob = 0.4
+    # hard limit at 100bp size
+    max_size = 100
+    del_prob = 0.5
+    ins_prob = 0.5
     #max_size = 15
     #max_size = 3
-    mut_size = int(np.random.exponential(6,1)[0])
-    while mut_size == 0 or mut_size > max_size:
-        mut_size = int(np.random.exponential(6,1)[0])
+    mut_size = math.ceil(np.random.exponential(6,1)[0])
+    while mut_size > max_size:
+        mut_size = math.ceil(np.random.exponential(6,1)[0])
     mut_type = random.choices(population=['ins','mut'],weights=[ins_prob,del_prob],k=1)[0]
     #mut_size = random.randint(1, 10)
     if mut_type == 'ins':
@@ -54,12 +136,12 @@ def generate_indel(max_size):
         mut_bases = mut_size * "-"
     return mut_bases
 
-def sim_chars(tree,mut_rate):
+def sim_chars(tree,mut_rate,num_cuts):
     np.random.seed(seed=None)
     lt_sim = cas.sim.Cas9LineageTracingDataSimulator(
-        number_of_cassettes = 5,
+        number_of_cassettes = num_cuts,
         size_of_cassette = 1,
-        mutation_rate = mut_rate,
+        mutation_rate = mut_rate, # can also be a list of rates of equal len to num_cuts
         state_generating_distribution = lambda: np.random.exponential(1e-5),
         number_of_states = 100,
         state_priors = None,
@@ -128,22 +210,37 @@ def assign_tissue_labels(cas_tree,trans_mat):
     return tissues_df, tree_labeled
     
     
+## PARAMETERS ##
 outprefix = sys.argv[1]
-high_mut_rate = float(sys.argv[2])
-low_mut_rate = float(sys.argv[3])
-max_size = int(sys.argv[4])
-sample_num = int(sys.argv[5])
-migration_matrix_filepath = str(sys.argv[6])
-
-#max_size = 3
+m = sys.argv[2]
+sample_num = int(sys.argv[3])
+migration_matrix_filepath = str(sys.argv[4])
+migration_matrix = pd.read_csv(migration_matrix_filepath, header=0, index_col=0).to_dict(orient='index')
 #migration_matrix = {"PRL":{"PRL":0.98,"HMR":0.01, "LGR":0.01},
 #                    "HMR":{"PRL":0.01,"HMR":0.98,"LGR":0.01},
 #                    "LGR":{"PRL":0.01,"HMR":0.01,"LGR":0.98}}
 #migration_matrix_filepath = "data/true_migration_prob_matrix.csv"
+num_cuts = 10
+ref_seq = "TCTACACGCGCGTTCAACCGAGGAAAACTACACACACGTTCAACCACGGTTTTTTACACACGCATTCAACCACGGACTGCTACACACGCACTCAACCGTGGATATTTACATACTCGTTCAACCGTGGATTGTTACACCCGCGTTCAACCAGGGTCAGATACACCCACGTTCAACCGTGGTACTATACTCGGGCATTCAACCGCGGCTTTCTGCACACGCCTACAACCGCGGAACTATACACGTGCATTCACCCGTGGATC"
+# positions are 1-indexed
+ref_cut_sites = [17, 43, 69, 95, 121, 147, 173, 199, 225, 251]
+try:
+    float(m)
+    m = [float(m)] * num_cuts
+except:
+    if len(m.split(",")) == num_cuts:
+        m = m.split(",")
+        m = [float(i) for i in m]
+    else:
+        print("Comma-separated list of mutation rates was not of len 1 or equal to len of num_cuts. Exiting!")
+        sys.exit()
 
-migration_matrix = pd.read_csv(migration_matrix_filepath, header=0, index_col=0).to_dict(orient='index')
+## SIMULATE TREE
+# patch Cassiopeia overlay function
+cas.sim.Cas9LineageTracingDataSimulator.overlay_data = overlay_data_drop
 
 # simulate a tree based on birth death model with num_extant leaves
+# information on tree object: https://cassiopeia-lineage.readthedocs.io/en/latest/api/reference/cassiopeia.data.CassiopeiaTree.html
 bd_sim = cas.sim.BirthDeathFitnessSimulator(
     birth_waiting_distribution = lambda scale: np.random.exponential(scale),
     initial_birth_scale = 0.5,
@@ -170,81 +267,106 @@ out_tree_tissues = outprefix + "_true_tissues.nwk"
 with open(out_tree_tissues,'w') as ttt:
     ttt.write(labeled_tree.get_newick())
 
+# Cassipeia can take a list of values for the mutation rate, applying a different rate to each site
+final_matrix = sim_chars(ground_truth_tree,m,num_cuts)
+site_names = list(final_matrix.columns)
+seq_arr = np.array(list(ref_seq))
+# alternative way to get ref cut sites but with unequal spacing due to rounding
+#ref_cut_sites = list(np.round(np.linspace(16, len(seq_arr) - 10, num_cuts)).astype(int))
 
+## EMPIRICAL CALC EVEN CUT SITES ##
+# How empirical cut sites in data were defined
+# This matches the hardcoded ref_seq above in terms of RNA guide sequences
+# number of base pairs buffer at start and end of amplicon
+#buffer1 = 17
+#buffer2 = 10
+# cut_distances guaranteed to be equal to prevent biases in dropout
+#cut_dist = int((len(seq_arr)-(0))/num_cuts)
+# number of cut sites guaranteed to equal num_cuts
+#ref_cut_sites = list(np.arange(buffer1, (len(seq_arr) - buffer2)+(buffer1+buffer2), cut_dist)[:num_cuts])
 
-#mut_rates = [0.1,0.01]
-mut_rates = [high_mut_rate,low_mut_rate]
-for i,m in enumerate(mut_rates):
-    if i != 0:
-        suffix = "_" + str(i) + "_" + str(m)
-        character_matrix = character_matrix.join(sim_chars(ground_truth_tree,m),rsuffix=suffix)
-    else:
-        character_matrix = sim_chars(ground_truth_tree,m) 
-# join the matrices on the index col that is the tree leaf label
-final_matrix = character_matrix
+## GENERIC CALC EVEN CUT SITES ##
+# Uncomment this to allow flexible number of cut sites
+# This will no longer match the RNA guide sequences in ref_seq with implications for alignment
+#buffer1 = 10
+#buffer2 = 10
+# cut_distances guaranteed to be equal to prevent biases in dropout
+#cut_dist = int((len(seq_arr)-(buffer1+buffer2))/num_cuts)
+# number of cut sites guaranteed to equal num_cuts
+#ref_cut_sites = list(np.arange(buffer1, (len(seq_arr) - buffer2), cut_dist)[:num_cuts])
+
+## SIMULATE MUTATIONS AND MUTANT SEQS ##
+sample_names = list(final_matrix.index)
+mut_dict = {}
+cut_dict = {}
+mut_ages = {}
+row_fasta = {}
+for index, row in final_matrix.iterrows():
+    for site_i,s in enumerate(site_names):
+        if "_" in str(row[s]):
+            clean_name = int(str(row[s].split("_")[0]))
+            age = float(str(row[s].split("_")[1]))
+            if clean_name not in mut_ages:
+                # note that age = amount of time that passed since divergence from root
+                # so the oldest mutations will have the lowest numerical value
+                mut_ages[clean_name] = age
+            # update matrix
+            final_matrix.at[index,s] = clean_name
+            # update row
+            row[s] = clean_name
+            # might not need cut_dict but keep for now
+            if s not in cut_dict:
+                cut_dict[s] = ref_cut_sites[site_i]
+    ages = []
+    mutations = list(row)
+    # get order of sites by time of mutation
+    for m in mutations:
+        if str(m) != '0':
+            age = mut_ages[m]
+        else:
+            age = 0
+        ages.append(age)
+    # sort from oldest to youngest
+    idx_by_age = np.argsort(ages)
+    # keep track of deleted sites
+    deleted_site_idx = []
+    # mutation range
+    amplicon_seq = ref_seq[:]
+    amplicon_cut_sites = ref_cut_sites[:]
+    for i in idx_by_age:
+        if i not in deleted_site_idx and ages[i] > 0:
+            mutation = mutations[i]
+            if mutation not in mut_dict:
+                mut_dict[mutation] = generate_indel()
+            mseq = mut_dict[mutation]
+            indel_len = len(mseq)
+            mpos = amplicon_cut_sites[i]
+            # if deletion
+            if "-" in mseq:
+                # add intersecting cut sites to deleted
+                for i_site,s in enumerate(amplicon_cut_sites):
+                    # interval comparison ignoring focal site
+                    if s != mpos and mpos <= s <= (mpos+indel_len):
+                        deleted_site_idx.append(i_site)
+                        # update final_matrix value to missing, here encoded as -1
+                        final_matrix.at[index,site_names[i_site]] = -1
+                # update amplicon_seq with deletion
+                amplicon_seq = mutate_seq(amplicon_seq,mpos,mseq)
+            # if insertion
+            else:
+                amplicon_seq = mutate_seq(amplicon_seq,mpos,mseq)
+                # shift amplicon cut_sites
+                for i_site,s in enumerate(amplicon_cut_sites):
+                    if s > mpos:
+                        # increment s by indel_len
+                        amplicon_cut_sites[i_site] = s + indel_len
+    row_fasta[index]=amplicon_seq
+# INFER TREE FROM SIMULATED MUTATIONS ## 
 reconstructed_tree = cas.data.CassiopeiaTree(character_matrix = final_matrix, missing_state_indicator = -1)
 greedy_solver = cas.solver.VanillaGreedySolver()
 greedy_solver.solve(reconstructed_tree)
 
-# Get the reconstructed tree newick
-#s2 = reconstructed_tree.get_newick() 
-
-# Resolve polytomy for RF calculation
-#t = Tree(reconstructed_tree.get_newick())
-#t.resolve_polytomy(recursive=True)
-#s2 = t.write()
-#nj_solver = cas.solver.NeighborJoiningSolver(add_root=False)
-
-# Calculate RF distance between true and reconstructed tree
-# https://dendropy.org/primer/treestats.html
-# establish common taxon namespace
-#tns = dendropy.TaxonNamespace()
-#taxa = dendropy.TaxonNamespace(reconstructed_tree.leaves)
-
-# ensure all trees loaded use common namespace
-#tree1 = dendropy.Tree.get(
-#        data=s1,
-#        schema='newick',
-#        taxon_namespace=taxa)
-#tree2 = dendropy.Tree.get(
-#        data=s2,
-#        schema='newick',
-#        taxon_namespace=taxa)
-#ntax = len(reconstructed_tree.leaves)
-#t = treesim.birth_death_tree(0.4, 0.1, taxon_namespace=taxa,num_extant_tips=ntax)
-
-
-# prepare mutated sequence
-ref_seq = "TCTACACGCGCGTTCAACCGAGGAAAACTACACACACGTTCAACCACGGTTTTTTACACACGCATTCAACCACGGACTGCTACACACGCACTCAACCGTGGATATTTACATACTCGTTCAACCGTGGATTGTTACACCCGCGTTCAACCAGGGTCAGATACACCCACGTTCAACCGTGGTACTATACTCGGGCATTCAACCGCGGCTTTCTGCACACGCCTACAACCGCGGAACTATACACGTGCATTCACCCGTGGATC"
-# positions are 1-indexed
-ref_cut_sites = [17, 43, 69, 95, 121, 147, 173, 199, 225, 251] 
-#ref_border_sites = [1, 26, 52, 78, 104, 130, 156, 182, 208, 234]
-site_names = list(final_matrix.columns)
-sample_names = list(final_matrix.index)
-site_indels = set()
-mut_dict = {}
-cut_dict = {}
-
-for m,s in enumerate(site_names):
-    cut_dict[s] = ref_cut_sites[m]
-    indel_list = final_matrix[s].tolist()
-    mutations = []
-    #print("checking site:",m,s)
-    for i in indel_list:
-        #print("checking indel:", i)
-        if str(i) != '0':
-            site_indels.add(i)
-            #print("Added to site_indel set:", site_indels)
-            if i not in mut_dict:
-                indel_seq = generate_indel(max_size)
-                #print("Indel not in mut_dict - generated indel:",indel_seq)
-                # ensure all indels at each site are unique
-                while indel_seq in mutations:
-                    #print("Indel seq",indel_seq,"already exists at site:",mutations)
-                    indel_seq = generate_indel(max_size)
-                #print("Adding new indel to site",indel_seq)
-                mutations.append(indel_seq)
-                mut_dict[i] = indel_seq
+## WRITE OUTPUT FILES ##
 
 # write trees, mutation matrix and dictionaries to output tables
 out_mut = outprefix + "_mutations.tsv"
@@ -252,7 +374,8 @@ out_cut = outprefix + "_cut_positions.tsv"
 out_matrix = outprefix + "_indel_character_matrix.tsv"
 with open(out_mut,'a') as outm:
     for k,v in mut_dict.items():
-        outline = str(k) + "\t" + str(v) + "\n"
+        age = mut_ages[k]
+        outline = str(k) + "\t" + str(v) + "\t" + str(age) + "\n"
         outm.write(outline)
 with open(out_cut,'a') as outc:
     for k,v in cut_dict.items():
@@ -268,7 +391,7 @@ with open(out_tree_true,'w') as tt:
 with open(out_tree_infer,'w') as it:
     it.write(reconstructed_tree.get_newick())
 
-# Write overall fasta file
+# Fasta sequence for each simulated sample
 outfasta = outprefix + ".fa"
 with open(outfasta,'a') as o:
     outstr = ">ref" + "\n" + ref_seq + "\n"
@@ -282,39 +405,12 @@ for tis in tissues:
         outstr = ">ref" + "\n" + ref_seq + "\n"
         o.write(outstr)
 
-
-for index, row in final_matrix.iterrows():
-    row_fasta = ref_seq
-    tissue = tissue_labels_df.loc[tissue_labels_df['node'] == index, 'tissue'].values[0]
-    #print("refseq",ref_seq)
-    #print(index,row)
-    insertion_pos_dict = {}
-    #print("ins_pos_dict",insertion_pos_dict)
-    for s in site_names:
-        indel_id = row[s]
-        cut_site = cut_dict[s]
-        # if not no indel
-        if indel_id != 0:
-            #print("Mutation at site")
-            mut_bases = mut_dict[indel_id]
-            #print("mut_bases:",mut_bases)
-            insertion_shift = 0
-            for k,v in insertion_pos_dict.items():
-                # preceding insertion
-                if k < cut_site:
-                    insertion_shift += len(v)
-            #print("Final insertion shift:",insertion_shift)
-            cut_site += insertion_shift
-            row_fasta = mutate_seq(row_fasta,cut_site,mut_bases)
-            #print("cut site,rowfasta", cut_site,row_fasta)
-            # track insertions
-            if '-' not in mut_bases:
-                insertion_pos_dict[cut_site] = mut_bases
-                #print("Added cut site to ins_pos_dict", cut_site, mut_bases)
-    row_fasta = row_fasta.replace('-', '')
-    outstr = ">" + index + "\n" + row_fasta + "\n"
+for i,seq in enumerate(row_fasta):
+    seq = seq.replace('-', '')
+    outstr = ">" + str(i) + "\n" + seq + "\n"
     with open(outfasta,'a') as o:
         o.write(outstr)
-        
+    
+    tissue = tissue_labels_df.loc[tissue_labels_df['node'] == i, 'tissue'].values[0]
     with open(globals()[f"outfasta_{tissue}"],'a') as t:
         t.write(outstr)
