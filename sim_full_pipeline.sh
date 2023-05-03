f082a160b3eab91ae44fc37105323763503848f5
Staklinski <staklins@evolgen.cshl.edu>
2023-05-03 10:08:25 -0400

    Merge stephen simulator code into main

--- sim_full_pipeline.sh
+++ sim_full_pipeline.sh
@@ -97,7 +97,7 @@ RAW_TISSUES=$(head -n 1 ${MIGRATION_MATRIX})
 IFS=', ' read -r -a TISSUES <<< "${RAW_TISSUES}"
 PTISSUE="${TISSUES[1]}"
 
-./run_pipeline.sh --infile ${ASV} --tree ${TREE} --scripts ${SPATH} --prefix ${PREFIX} --primary-tissue ${PTISSUE}
+timeout 3m ./run_pipeline.sh --infile ${ASV} --tree ${TREE} --scripts ${SPATH} --prefix ${PREFIX} --primary-tissue ${PTISSUE} --keep-first-cp
 
 mkdir ${outputdir}${PREFIX}
 mv ${PREFIX}_cp_output/* ${outputdir}${PREFIX}/
@@ -111,6 +111,7 @@ conda deactivate
 mv ${sim_dir}${NAME}_true_tissues.nwk ${outputdir}
 mv ${sim_dir}${NAME}_tissues.tsv ${outputdir}
 mv ${sim_dir}${NAME}_mutations.tsv ${outputdir}
+mv ${sim_dir}${NAME}_indel_character_matrix.tsv ${outputdir}
 rm -r ${sim_dir}
 
 # Prune all EvoTraceR output files
@@ -133,12 +134,46 @@ inferred_count=$(awk -F',' 'NR>1{sum+=$3} END{print sum}' ${outputdir}${PREFIX}_
 #echo "Total inferred migrations in the ${NAME} simulation: $inferred_count"
 proportion=$(echo "scale=4;$inferred_count/$true_count" | bc)
 
-num_mutations=$(wc -l ${outputdir}${NAME}_mutations.tsv | awk '{print $1}')
+num_uniq_mutations=$(wc -l ${outputdir}${NAME}_mutations.tsv | awk '{print $1}')
+
+avg_mut_age=$(awk '{ age += $NF } END { print age / NR }' ${outputdir}${NAME}_mutations.tsv)
 
 # Write both true and inferred to an output csv with the input parameters stored
-echo "name,mutrate,num_samples,migration_matrix,num_mutations,true_migrations,inferred_migrations,proportion" >> ${outputdir}comparison_inferred_true_migration_${NAME}.csv
+echo "name,mutrate,num_samples,migration_matrix,num_uniq_mutations,uniq_mut_per_site,total_mut_sites,avg_mut_sites_per_sample,avg_proportion_mut_sites,total_dropout,dropout_per_sample,avg_mutation_age,true_migrations,inferred_migrations,proportion" >> ${outputdir}comparison_inferred_true_migration_${NAME}.csv
 mr_write=${MUTRATE//,/ }
-echo "${NAME},${mr_write},${NUM_SAMPLES},${MIGRATION_MATRIX},${num_mutations},${true_count},${inferred_count},${proportion}" >> ${outputdir}comparison_inferred_true_migration_${NAME}.csv
+# Loop over the array and count the non-zero values
+IFS=' ' read -r -a mut_array <<< "$mr_write"
+num_sites=0
+for i in "${mut_array[@]}"
+do
+  if [[ $i != '0' ]]
+  then
+    num_sites=$((num_sites+1))
+  fi   
+done
+uniq_mut_per_site=$(echo "scale=4; $num_uniq_mutations / $num_sites" | bc)
+
+# Initialize a variable to keep track of the total number of -1 values
+total_dropout=0
+declare -a dropout_array
+total_mut_sites=0
+declare -a mut_array
+
+while read line; do
+    # Count the number of -1 values in the row
+    row_dropout=$(echo "$line" | cut -f 2- | tr '\t' '\n' | grep -e "^-1$" | wc -l)
+    total_dropout=$((total_dropout + row_dropout))
+    dropout_array+=("$row_dropout")
+    # Count non 0 and non -1 to get number of mutated sites per row
+    row_mut_count=$(echo "$line" | cut -f 2- | tr '\t' '\n' | grep -v -e "^$" -e "^-1$" -e "^0$" | wc -l)
+    total_mut_sites=$((total_mut_sites + row_mut_count))
+    mut_array+=("$row_mut_count")
+done < <(tail -n +2 "${outputdir}${NAME}_indel_character_matrix.tsv")
+avg_row_dropout=$(echo "scale=4; $total_dropout / $NUM_SAMPLES" | bc)
+avg_mut_sites_per_sample=$(echo "scale=4; $total_mut_sites / $NUM_SAMPLES" | bc)
+avg_proportion_mut_sites=$(echo "scale=4; $avg_mut_sites_per_sample / $num_sites" | bc)
+
+echo "${NAME},${mr_write},${NUM_SAMPLES},${MIGRATION_MATRIX},${num_uniq_mutations},${uniq_mut_per_site},${total_mut_sites},${avg_mut_sites_per_sample},${avg_proportion_mut_sites},${total_dropout},${avg_row_dropout},${avg_mut_age},${true_count},${inferred_count},${proportion}" >> ${outputdir}comparison_inferred_true_migration_${NAME}.csv
 
 conda activate simulate
 
