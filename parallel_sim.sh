089be9f172b9d17780417ad822d3d3ee06edf5ea
Staklinski <staklins@evolgen.cshl.edu>
2023-07-17 13:20:04 -0400

    update to merge

--- parallel_sim.sh
+++ parallel_sim.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source ~/miniconda3/etc/profile.d/conda.sh
 
-parallel_sim_name="evoCapPaper_metastatic_trajectories_evoMachina_7.15.23"
+parallel_sim_name="variableMutrateMigrationRepeat_evoMachina_7.15.23"
 par_results="${parallel_sim_name}_parallel_sim_results"
 mkdir ${par_results}
 mkdir ${par_results}/data
 
 # Set the mutation rates to explore
-mr1=(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
+# mr1=(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
 # mr2=(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0)
 # mr3=(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0,0)
 # mr4=(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0,0,0)
@@ -17,15 +17,15 @@ mr1=(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
 # mr8=(0.1,0.1,0.1,0,0,0,0,0,0,0)
 # mr9=(0.1,0.1,0,0,0,0,0,0,0,0)
 # mr10=(0.1,0,0,0,0,0,0,0,0,0)
-# mr11=(0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01)
-# mr12=(0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05)
-# mr13=(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
-# mr14=(0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15)
-# mr15=(0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2)
-# mr16=(0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3)
-# mr17=(0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5)
-# mr18=(0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7)
-# mr19=(1,1,1,1,1,1,1,1,1,1)
+mr1=(0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01)
+mr2=(0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05)
+mr3=(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
+mr4=(0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15,0.15)
+mr5=(0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2)
+mr6=(0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3)
+mr7=(0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5)
+mr8=(0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7)
+mr9=(1,1,1,1,1,1,1,1,1,1)
 # mr20=(1,0.01,1,0.01,1,0.01,1,0.01,1,0.01)
 # mr21=(1,1,1,1,0.1,0.1,0.1,0.01,0.01,0.01)
 # mr22=(1,1,1,1,1,0.01,0.01,0.01,0.01,0.01)
@@ -34,12 +34,12 @@ mr1=(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
 # mr25=(1,0.01,0.01,1,0.01,0.01,1,0.01,0.01,1)
 # mr26=(1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
 # mr27=(0.1,0.01,0.1,0.01,0.1,0.01,0.1,0.01,0.1,0.01)
-# mr_array=("$mr1" "$mr2" "$mr3" "$mr4" "$mr5" "$mr6" "$mr7" "$mr8" "$mr9" "$mr10" "$mr11" "$mr12" "$mr13" "$mr14" "$mr15" "$mr16" "$mr17" "$mr18" "$mr19" "$mr20" "$mr21" "$mr22" "$mr23" "$mr24" "$mr25" "$mr26" "$mr27")
+mr_array=("$mr1" "$mr2" "$mr3" "$mr4" "$mr5" "$mr6" "$mr7" "$mr8" "$mr9")
 
 ### mr input for mied strategies with an average of 0.1264
 #mr1=(0.1264,0.1264,0.1264,0.1264,0.1264,0.1264,0.1264,0.1264,0.1264,0.1264)
 #mr2=(0.3173305,0.0009823,0.00000008,0.00000162,0.00000335,0.18609872,0.00000254,0,0,0)
-mr_array=("$mr1")
+#mr_array=("$mr1")
 
 ### Use below to take input mutrate for scipy.optimize script
 #while [[ "$#" -gt 0 ]]; do
@@ -56,18 +56,18 @@ ss_array=(100)
 
 # Set the migration matrix values that will be tested
 # ultra_rare_mm="data/ultrarare_migration_prob_matrix.csv"
-# rare_mm="data/rare_migration_prob_matrix.csv"
-# moderate_mm="data/moderate_migration_prob_matrix.csv"
-# true_mm="data/true_migration_prob_matrix.csv"
-# high_mm="data/high_migration_prob_matrix.csv"
-# equal_mm="data/equal_migration_prob_matrix.csv"
-# #mm_array=(${rare_mm} ${equal_mm} ${moderate_mm} ${high_mm} ${true_mm})
+rare_mm="data/rare_migration_prob_matrix.csv"
+#moderate_mm="data/moderate_migration_prob_matrix.csv"
+true_mm="data/true_migration_prob_matrix.csv"
+high_mm="data/high_migration_prob_matrix.csv"
+#equal_mm="data/equal_migration_prob_matrix.csv"
+mm_array=(${rare_mm} ${true_mm} ${high_mm})
 # mm_array=(${true_mm})
 
-primary_mono_seeding_mm="data/primary_mono_seeding_migration_prob_matrix.csv"
-primary_re_seeding_mm="data/primary_reseeding_migration_prob_matrix.csv"
-metastatic_re_seeding_mm="data/metastatic_reseeding_migration_prob_matrix.csv"
-mm_array=(${primary_mono_seeding_mm} ${primary_re_seeding_mm} ${metastatic_re_seeding_mm})
+# primary_mono_seeding_mm="data/primary_mono_seeding_migration_prob_matrix.csv"
+# primary_re_seeding_mm="data/primary_reseeding_migration_prob_matrix.csv"
+# metastatic_re_seeding_mm="data/metastatic_reseeding_migration_prob_matrix.csv"
+# mm_array=(${primary_mono_seeding_mm} ${primary_re_seeding_mm} ${metastatic_re_seeding_mm})
 
 
 # Setup headers for recording the input parameters in a csv
@@ -156,7 +156,7 @@ do
   for f in sim_results*/comparison*; do tail -n +2 $f >> output_all_${parallel_sim_name}.csv; done
   # Remove all the individual csv files
   #rm -r sim_results*
-  mv -r sim_results* ${par_results}/data/
+  mv sim_results* ${par_results}/data/
 done
 
 echo "All batches are done."
